0b7aa33fa567594d0a55502b76d4704035b16c10
Lili Boxer <ninaliboxer@gmail.com>
2019-09-05 11:58:17 -0700

    auth0provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import Auth0Provider from './Auth0Provider';
 
 render(
-  <App />,
+  <Auth0Provider
+    domain="maxthecat.auth0.com"
+    client_id="pHgjOz114ivNWWxCLidIn4SDpfm1zKAy"
+    redirect_uri="http://localhost:7890"
+  >
+    <App />
+  </Auth0Provider>,
   document.getElementById('root')
 );
